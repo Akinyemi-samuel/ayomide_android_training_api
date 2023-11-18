6c2337b452bae5a422719738bfb209403c94e42e
samuel <samfrosh673@gmail.com>
2023-11-19 00:09:10 +0100

    admin can get user-detail when authenticated

--- src/main/java/com/samuelClass/dto/request/RegistrationDto.java
+++ src/main/java/com/samuelClass/dto/request/RegistrationDto.java
@@ -1,8 +1,14 @@
 package com.samuelClass.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
+@AllArgsConstructor
 public class RegistrationDto {
 
     public String firstName;
